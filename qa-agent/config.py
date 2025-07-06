865ac0a2b9cd1992a5c1ee5f1fbe961d6bbed213
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2025-07-06 20:53:16 +0530

    Update config.py

--- qa-agent/config.py
+++ qa-agent/config.py
@@ -49,3 +49,22 @@ LLM_CONFIG = {
 
 # Default video URL for testing
 DEFAULT_VIDEO_URL = "https://www.youtube.com/watch?v=IK62Rk47aas"
+
+from youtube_transcript_api import YouTubeTranscriptApi as yta
+
+link = 'https://www.youtube.com/watch?v=IK62Rk47aas'
+id = link.split('=')
+vid_id = id[-1]
+
+data = yta.get_transcript(vid_id)
+# print(data)
+
+final_data = ''
+for val in data:
+    for key,value in val.items():
+        if key == 'text':
+            final_data += value
+process_data = final_data.splitlines()
+clean_data = ''.join(process_data)
+
+print(clean_data)
